9c14765d2e93a9f61302c6570803b59a4cd74853
Ivan Serebrennikov <admin@silberworks.com>
2023-10-08 16:34:20 +0300

    feat(utils): change aes

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,30 +1,37 @@
+import os
 import pyaes
 import base64
 
 
-def aes_encode(key: bytes, iv: bytes, data: str) -> str:
+def aes_encode(data: str, key: str) -> str:
     """
-    key: 16, 24, 32 bytes sync encrypt key
-    iv: initial vector - 16 bytes
-    data: python str
-    return: base64 str - utf-8
+    data: any python str
+    key: (base64 str) 16, 24, 32 bytes sync encrypt key
+    return: (base64 str - iv).(base64 str - encrypted data)
     """
+    key = base64.b64decode(key.encode())
+    iv = os.urandom(16)
 
     # set encrypter
     encrypter = pyaes.Encrypter(pyaes.AESModeOfOperationCBC(key, iv))
+    # encrypted binary to base64 str
+    cipher = base64.b64encode(encrypter.feed(data) + encrypter.feed()).decode('utf-8')
 
-    return base64.b64encode(encrypter.feed(data) + encrypter.feed()).decode('utf-8')
+    return f"{base64.b64encode(iv).decode('utf-8')}.{cipher}"
 
 
-def aes_decode(key: bytes, iv: bytes, cipher: str) -> str:
+def aes_decode(data: str, key: str) -> str:
     """
-    key: 16, 24, 32 bytes sync encrypt key
-    iv: initial vector - 16 bytes
-    data: base64 str - utf-8
-    return: python str
+    data: (base64 str - iv).(base64 str - encrypted data)
+    key: (base64 str) 16, 24, 32 bytes sync encrypt key
+    return: decode python str
     """
+    key = base64.b64decode(key.encode())
+    iv = base64.b64decode(data.split('.')[0].encode())
 
     # set decrypter
     decrypter = pyaes.Decrypter(pyaes.AESModeOfOperationCBC(key, iv))
+    # data (iv).(encrypted text) to binary encrypted text
+    cipher = base64.b64decode(data.split('.')[1].encode())
 
-    return decrypter.feed(base64.b64decode(cipher)) + decrypter.feed()
+    return (decrypter.feed(cipher) + decrypter.feed()).decode('utf-8')
