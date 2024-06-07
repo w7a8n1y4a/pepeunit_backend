7b9d6072a008349232833a5cee45e8984693196a
Ivan Serebrennikov <admin@silberworks.com>
2024-06-07 05:29:38 +0300

    feat(ci): add entrypoint and dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+alembic upgrade head
+gunicorn app.main:app \
+    --bind 0.0.0.0:5000 \
+    --log-level 'warning' \
+    --access-logfile /dev/stdout \
+    --error-logfile /dev/stderr \
+    --timeout 300 \
+    --workers=1 \
+    --threads=2 \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --worker-tmp-dir=/dev/shm \
+    --preload \
+    --max-requests=1000 \
+    --max-requests-jitter=100
