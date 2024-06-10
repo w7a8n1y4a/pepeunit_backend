61c3ffeea56d1b06aa2afe7388f0d4a26f866517
Ivan Serebrennikov <admin@silberworks.com>
2024-06-11 01:44:42 +0300

    feat(tests): change env, add valid end, add first script for repo

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,5 +1,9 @@
+import json
+import os.path
+import shutil
 from collections import namedtuple
 import hashlib
+from json import JSONDecodeError
 
 import pytest
 
@@ -11,16 +15,25 @@ from app.domain.unit_node_model import UnitNode
 from app.domain.user_model import User
 from sqlmodel import Session
 
+from app.schemas.pydantic.repo import Credentials
+
 test_hash = hashlib.md5(settings.backend_domain.encode('utf-8')).hexdigest()
 
 Info = namedtuple('Info', "context")
 
 
 def pytest_configure():
+    # search hash for db
     pytest.test_hash = test_hash
+
+    # last - Admin, first - User: minimal 2 users
     pytest.users = []
+
+    # {uuid_user: jwt-token}: minimal 2 items
     pytest.user_tokens_dict = {}
 
+    # ['Private', 'Private', 'Public', 'Public'] : minimal 4 repo
+    pytest.repos = []
 
 @pytest.fixture(scope="session")
 def database() -> Session:
@@ -33,10 +46,19 @@ def clear_database(database) -> None:
     clear all entity in database with test_hash in field
     """
 
-    database.query(User).where(User.login.ilike(f'%{test_hash}%')).delete()
-    database.query(Repo).where(Repo.name.ilike(f'%{test_hash}%')).delete()
     database.query(Unit).where(Unit.name.ilike(f'%{test_hash}%')).delete()
-    database.query(UnitNode).where(UnitNode.topic_name.ilike(f'%{test_hash}%')).delete()
+
+    # clear physical repos
+    repos = database.query(Repo).where(Repo.name.ilike(f'%{test_hash}%')).all()
+    for repo in repos:
+        target_del_path = f'{settings.save_repo_path}/{str(repo.uuid)}'
+        try:
+            shutil.rmtree(target_del_path)
+        except FileNotFoundError:
+            pass
+
+    database.query(Repo).where(Repo.uuid.in_([repo.uuid for repo in repos])).delete()
+    database.query(User).where(User.login.ilike(f'%{test_hash}%')).delete()
     database.commit()
 
 
@@ -48,4 +70,35 @@ def test_users() -> list[dict]:
             "password": f'test{inc}',
         }
         for inc in range(2)
+    ]
+
+
+@pytest.fixture()
+def test_repos() -> list[dict]:
+
+    # get private repository
+    test_repos = []
+    try:
+        data = json.loads(settings.test_private_repo_json)
+        test_repos.extend(data['data'])
+    except JSONDecodeError:
+        pass
+
+    # add public repository
+    test_repos.extend([
+        {'type': 'Public', 'link': 'https://git.pepemoss.com/pepe/pepeunit/units/gitlab_unit_pub_test.git'},
+        {'type': 'Public', 'link': 'https://github.com/w7a8n1y4a/github_unit_pub_test.git'}
+    ])
+
+    return [
+        {
+            'visibility_level': 'Public'  if repo['type'] == 'Public' else 'Private',
+            'name': f'test_{inc}_{test_hash}',
+            'repo_url': repo['link'],
+            'is_public_repository': True if repo['type'] == 'Public' else False,
+            'credentials': None if repo['type'] == 'Public' else Credentials(username=repo['username'], pat_token=repo['pat_token']),
+            'is_auto_update_repo': True,
+            'update_frequency_in_seconds':  86400
+        }
+        for inc, repo in enumerate(test_repos)
     ]
---
+++ tests/integration/services/9_end_test.py
@@ -0,0 +1,10 @@
+import pytest
+
+from app import settings
+
+
+if settings.test_clear_data:
+
+    @pytest.mark.last
+    def test_end(clear_database):
+        assert True
