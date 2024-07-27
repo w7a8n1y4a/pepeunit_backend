f9cd52bbd53a4e41cdc959ae25892a0ef56be4a1
Ivan Serebrennikov <admin@silberworks.com>
2024-07-27 21:46:56 +0300

    refactor(permission): change model and all logic, fix delete problem

--- app/domain/permission_model.py
+++ app/domain/permission_model.py
@@ -1,6 +1,12 @@
 import uuid as pkg_uuid
+from typing import Optional
 
+from pydantic import BaseModel
+from sqlalchemy import ForeignKey, Column
+from sqlalchemy.dialects.postgresql import UUID
 from sqlmodel import SQLModel, Field
+
+from app.repositories.enum import PermissionEntities
 
 
 class Permission(SQLModel, table=True):
@@ -10,10 +16,34 @@ class Permission(SQLModel, table=True):
 
     uuid: pkg_uuid.UUID = Field(primary_key=True, nullable=False, index=True, default_factory=pkg_uuid.uuid4)
 
-    agent_uuid: pkg_uuid.UUID = Field(nullable=False, index=True)
     # User, Unit, Unit Node
     agent_type: str = Field(nullable=False)
+    agent_user_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('users.uuid', ondelete='CASCADE'), nullable=True)
+    )
+    agent_unit_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('units.uuid', ondelete='CASCADE'), nullable=True)
+    )
+    agent_unit_node_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('units_nodes.uuid', ondelete='CASCADE'), nullable=True)
+    )
 
-    resource_uuid: pkg_uuid.UUID = Field(nullable=False, index=True)
     # Repo, Unit, UnitNode
     resource_type: str = Field(nullable=False)
+    resource_repo_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('repos.uuid', ondelete='CASCADE'), nullable=True)
+    )
+    resource_unit_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('units.uuid', ondelete='CASCADE'), nullable=True)
+    )
+    resource_unit_node_uuid: pkg_uuid.UUID = Field(
+        sa_column=Column(UUID(as_uuid=True), ForeignKey('units_nodes.uuid', ondelete='CASCADE'), nullable=True)
+    )
+
+
+class PermissionBaseType(BaseModel):
+    agent_uuid: Optional[pkg_uuid.UUID] = None
+    agent_type: Optional[PermissionEntities] = None
+
+    resource_uuid: Optional[pkg_uuid.UUID] = None
+    resource_type: Optional[PermissionEntities] = None
--- app/repositories/permission_repository.py
+++ app/repositories/permission_repository.py
@@ -1,10 +1,13 @@
+from typing import Union, Optional
+import uuid as pkg_uuid
+
 from fastapi import Depends, HTTPException
 from fastapi import status as http_status
 
 from sqlmodel import Session
 
 from app.configs.db import get_session
-from app.domain.permission_model import Permission
+from app.domain.permission_model import Permission, PermissionBaseType
 from app.domain.repo_model import Repo
 from app.domain.unit_model import Unit
 from app.domain.unit_node_model import UnitNode
@@ -18,50 +21,124 @@ class PermissionRepository:
     def __init__(self, db: Session = Depends(get_session)) -> None:
         self.db = db
 
-    def get(self, permission: Permission) -> Permission:
+    def get(self, permission: Permission) -> Optional[Permission]:
         return self.db.get(Permission, permission.uuid)
 
-    def get_agent(self, permission: Permission):
-        return self.db.get(eval(permission.agent_type), permission.agent_uuid)
+    def get_agent(self, base_permission: PermissionBaseType):
+        return self.db.get(eval(base_permission.agent_type), base_permission.agent_uuid)
 
-    def get_resource(self, permission: Permission):
-        return self.db.get(eval(permission.resource_type), permission.resource_uuid)
+    def get_resource(self, base_permission: PermissionBaseType):
+        return self.db.get(eval(base_permission.resource_type), base_permission.resource_uuid)
 
-    def create(self, permission: Permission) -> Permission:
+    @staticmethod
+    def base_type_to_domain(base_permission: PermissionBaseType) -> Permission:
+
+        permission = Permission(agent_type=base_permission.agent_type, resource_type=base_permission.resource_type)
+
+        agent_type = eval(base_permission.agent_type)
+        if agent_type is User:
+            permission.agent_user_uuid = base_permission.agent_uuid
+        elif agent_type is Unit:
+            permission.agent_unit_uuid = base_permission.agent_uuid
+        elif agent_type is UnitNode:
+            permission.agent_unit_node_uuid = base_permission.agent_uuid
+
+        resource_type = eval(base_permission.resource_type)
+        if resource_type is Repo:
+            permission.resource_repo_uuid = base_permission.resource_uuid
+        elif resource_type is Unit:
+            permission.resource_unit_uuid = base_permission.resource_uuid
+        elif resource_type is UnitNode:
+            permission.resource_unit_node_uuid = base_permission.resource_uuid
+
+        return permission
+
+    @staticmethod
+    def domain_to_base_type(permission: Permission) -> PermissionBaseType:
+
+        base_permission = PermissionBaseType(
+            agent_type=permission.agent_type,
+            resource_type=permission.resource_type
+        )
+
+        agent_type = eval(base_permission.agent_type)
+        if agent_type is User:
+            base_permission.agent_uuid = permission.agent_user_uuid
+        elif agent_type is Unit:
+            base_permission.agent_uuid = permission.agent_unit_uuid
+        elif agent_type is UnitNode:
+            base_permission.agent_uuid = permission.agent_unit_node_uuid
+
+        resource_type = eval(base_permission.resource_type)
+        if resource_type is Repo:
+            base_permission.resource_uuid = permission.resource_repo_uuid
+        elif resource_type is Unit:
+            base_permission.resource_uuid = permission.resource_unit_uuid
+        elif resource_type is UnitNode:
+            base_permission.resource_uuid = permission.resource_unit_node_uuid
+
+        return base_permission
+
+    def create(self, base_permission: PermissionBaseType) -> PermissionBaseType:
+
+        permission = self.base_type_to_domain(base_permission)
+
         self.db.add(permission)
         self.db.commit()
         self.db.refresh(permission)
-        return permission
+        return self.domain_to_base_type(permission)
 
-    def bulk_save(self, permissions: list[Permission]) -> None:
-        self.db.bulk_save_objects(permissions)
+    def bulk_save(self, base_permissions: list[PermissionBaseType]) -> None:
+        self.db.bulk_save_objects([self.base_type_to_domain(permission) for permission in base_permissions])
         self.db.commit()
 
-    def get_agent_resources(self, permission: Permission) -> list[str]:
+    @staticmethod
+    def get_agent_fld_uuid_by_type(fld_type: PermissionEntities) -> pkg_uuid.UUID:
+        entities_dict = {
+            PermissionEntities.USER: Permission.agent_user_uuid,
+            PermissionEntities.UNIT: Permission.agent_unit_uuid,
+            PermissionEntities.UNIT_NODE: Permission.agent_unit_node_uuid
+        }
+        return entities_dict[fld_type]
+
+    @staticmethod
+    def get_resource_fld_uuid_by_type(fld_type: PermissionEntities) -> pkg_uuid.UUID:
+        entities_dict = {
+            PermissionEntities.REPO: Permission.resource_repo_uuid,
+            PermissionEntities.UNIT: Permission.resource_unit_uuid,
+            PermissionEntities.UNIT_NODE: Permission.resource_unit_node_uuid
+        }
+        return entities_dict[fld_type]
+
+    def get_agent_resources(self, base_permission: PermissionBaseType) -> list[PermissionBaseType]:
+
         permissions = self.db.query(Permission).filter(
-            Permission.agent_uuid == permission.agent_uuid,
+            self.get_agent_fld_uuid_by_type(base_permission.agent_type) == base_permission.agent_uuid
         )
 
-        if permission.resource_type:
-            permissions = permissions.filter(Permission.resource_type == permission.resource_type)
+        if base_permission.resource_type:
+            permissions = permissions.filter(Permission.resource_type == base_permission.resource_type)
 
-        return permissions.all()
+        return [self.domain_to_base_type(permission) for permission in permissions.all()]
 
-    def get_resource_agents(self, permission: Permission):
+    def get_resource_agents(self, base_permission: PermissionBaseType) -> list[PermissionBaseType]:
+
         permissions = self.db.query(Permission).filter(
-            Permission.resource_uuid == permission.resource_uuid,
+            self.get_resource_fld_uuid_by_type(base_permission.resource_type) == base_permission.resource_uuid,
         )
 
-        if permission.agent_type:
-            permissions = permissions.filter(Permission.agent_type == permission.agent_type)
+        if base_permission.agent_type:
+            permissions = permissions.filter(Permission.agent_type == base_permission.agent_type)
 
-        return permissions.all()
+        return [self.domain_to_base_type(permission) for permission in permissions.all()]
 
-    def check(self, permission: Permission) -> bool:
+    def check(self, base_permission: PermissionBaseType) -> bool:
+
         check = (
             self.db.query(Permission)
             .filter(
-                Permission.agent_uuid == permission.agent_uuid, Permission.resource_uuid == permission.resource_uuid
+                self.get_agent_fld_uuid_by_type(base_permission.agent_type) == base_permission.agent_uuid,
+                self.get_resource_fld_uuid_by_type(base_permission.resource_type) == base_permission.resource_uuid
             )
             .first()
         )
@@ -69,18 +146,9 @@ class PermissionRepository:
         return bool(check)
 
     def delete(self, permission: Permission) -> None:
-        self.db.query(Permission).filter(
-            Permission.agent_uuid == permission.agent_uuid, Permission.resource_uuid == permission.resource_uuid
-        ).delete()
+        self.db.delete(self.get(permission))
         self.db.commit()
-
-    def delete_by_resource(self, permission: Permission) -> None:
-        self.db.query(Permission).filter(Permission.resource_uuid == permission.resource_uuid).delete()
-        self.db.commit()
-
-    def delete_by_agent(self, permission: Permission) -> None:
-        self.db.query(Permission).filter(Permission.agent_uuid == permission.agent_uuid).delete()
-        self.db.commit()
+        self.db.flush()
 
     @staticmethod
     def is_valid_agent_type(permission: Permission) -> None:
