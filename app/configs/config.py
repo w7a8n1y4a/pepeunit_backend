77c87837ee55594205849f1d975863f07f2f1308
Ivan Serebrennikov <admin@silberworks.com>
2025-02-02 06:29:45 +0300

    feat(ci): config add default state

--- app/configs/config.py
+++ app/configs/config.py
@@ -10,9 +10,9 @@ with open("pyproject.toml", "r") as f:
 class Settings(BaseSettings):
     """.env variables"""
 
-    debug: bool
-    app_prefix: str
-    api_v1_prefix: str
+    debug: bool = False
+    app_prefix: str = '/pepeunit'
+    api_v1_prefix: str = '/api/v1'
     project_name: str = data['tool']['poetry']['name']
     version: str = data['tool']['poetry']['version']
     description: str = data['tool']['poetry']['description']
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
     backend_link_prefix: str = ''
     backend_link_prefix_and_v1: str = ''
 
-    auth_token_expiration: int
+    auth_token_expiration: int = 2678400
     sqlalchemy_database_url: str
 
     secret_key: str
@@ -42,20 +42,20 @@ class Settings(BaseSettings):
     mqtt_host: str
     mqtt_secure: bool = True
     mqtt_http_type: str = 'https'
-    mqtt_port: int
+    mqtt_port: int = 1883
     mqtt_api_port: int = 18083
-    mqtt_keepalive: int
+    mqtt_keepalive: int = 60
     mqtt_username: str
     mqtt_password: str
     mqtt_max_payload_size: int = 50000
 
-    redis_url: str
-    redis_mqtt_auth_url: str
+    redis_url: str = 'redis://redis:6379/0'
+    redis_mqtt_auth_url: str = 'redis://redis:6379/0'
 
     available_topic_symbols: str = string.ascii_letters + string.digits + '/_-'
     available_name_entity_symbols: str = string.ascii_letters + string.digits + '_-.'
     available_password_symbols: str = string.ascii_letters + string.digits + string.punctuation
-    state_send_interval: int = 300
+    state_send_interval: int = 60
     max_external_repo_size: int = 50
     max_cipher_length: int = 50000
 
