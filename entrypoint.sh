505b59aa621f6906b15b301c7bd607134a3d7600
Ivan Serebrennikov <admin@silberworks.com>
2025-02-03 20:13:02 +0300

    feat(ci): entrypoint with wait db

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+DB_USER=$(echo "$SQLALCHEMY_DATABASE_URL" | sed -E 's|.*://([^:]+):.*|\1|')
+DB_PASS=$(echo "$SQLALCHEMY_DATABASE_URL" | sed -E 's|.*://[^:]+:([^@]+)@.*|\1|')
+DB_HOST=$(echo "$SQLALCHEMY_DATABASE_URL" | sed -E 's|.*@([^:/]+):.*|\1|')
+DB_PORT=$(echo "$SQLALCHEMY_DATABASE_URL" | sed -E 's|.*:([0-9]+)/.*|\1|')
+DB_NAME=$(echo "$SQLALCHEMY_DATABASE_URL" | sed -E 's|.*/([^/?]+).*|\1|')
+
+export PGPASSWORD="$DB_PASS"
+
+echo "Wait Ready PostgreSQL..."
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
+  sleep 2
+done
+echo "PostgreSQL available."
+
+echo "Wiat check DB '$DB_NAME'..."
+until psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d postgres -tAc \
+      "SELECT 1 FROM pg_database WHERE datname='$DB_NAME'" | grep -q 1; do
+  sleep 2
+done
+echo "DB '$DB_NAME' Exist."
+
+echo "Run migration..."
+
 alembic upgrade head
 gunicorn app.main:app \
     --bind 0.0.0.0:5000 \
