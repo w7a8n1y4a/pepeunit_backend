e3c577897ffad36de6a7c9af9fbb2f19849f9379
Ivan Serebrennikov <admin@silberworks.com>
2025-02-11 02:41:47 +0300

    feat(cipher): switch aes cbc to gcm

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -6,6 +6,7 @@ import string
 import time
 
 import pyaes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from pathspec import PathSpec
 from pathspec.patterns import GitWildMatchPattern
 
@@ -56,6 +57,43 @@ def aes_decode(data: str, key: str = settings.backend_encrypt_key) -> str:
     return (decrypter.feed(cipher) + decrypter.feed()).decode('utf-8')
 
 
+def aes_gcm_encode(data: str, key: str = settings.backend_encrypt_key) -> str:
+    """
+    data: any python str
+    key: (base64 str) 16, 24, 32 bytes sync encrypt key
+    return: (base64 str - nonce).(base64 str - encrypted data).(base64 str - tag)
+    """
+    len_content = len(data)
+    if len_content > settings.backend_max_cipher_length:
+        app_errors.cipher_error.raise_exception(
+            f'The encryption content is {len_content} long, although only <= {settings.backend_max_cipher_length} is allowed'
+        )
+
+    key = base64.b64decode(key.encode())
+    nonce = os.urandom(12)  # 96-bit nonce for AES-GCM
+    aesgcm = AESGCM(key)
+
+    cipher = aesgcm.encrypt(nonce, data.encode(), None)  # Encrypt data
+
+    return f"{base64.b64encode(nonce).decode()}.{base64.b64encode(cipher).decode()}"
+
+
+def aes_gcm_decode(data: str, key: str = settings.backend_encrypt_key) -> str:
+    """
+    data: (base64 str - nonce).(base64 str - encrypted data)
+    key: (base64 str) 16, 24, 32 bytes sync encrypt key
+    return: decode python str
+    """
+    key = base64.b64decode(key.encode())
+    nonce, cipher = data.split('.')
+    nonce = base64.b64decode(nonce.encode())
+    cipher = base64.b64decode(cipher.encode())
+
+    aesgcm = AESGCM(key)
+
+    return aesgcm.decrypt(nonce, cipher, None).decode('utf-8')
+
+
 def password_to_hash(password: str) -> (str, str):
     dynamic_salt = base64.b64encode(os.urandom(16)).decode('utf-8')
 
@@ -63,11 +101,11 @@ def password_to_hash(password: str) -> (str, str):
         'sha256', password.encode('utf-8'), (dynamic_salt + settings.backend_static_salt).encode('utf-8'), 100000
     )
 
-    return aes_encode(dynamic_salt), base64.b64encode(hashed_password).decode('utf-8')
+    return aes_gcm_encode(dynamic_salt), base64.b64encode(hashed_password).decode('utf-8')
 
 
 def check_password(password: str, hashed_password_db: str, cipher_dynamic_salt: str) -> bool:
-    dynamic_salt = aes_decode(cipher_dynamic_salt)
+    dynamic_salt = aes_gcm_decode(cipher_dynamic_salt)
 
     hashed_password = hashlib.pbkdf2_hmac(
         'sha256', password.encode('utf-8'), (dynamic_salt + settings.backend_static_salt).encode('utf-8'), 100000
