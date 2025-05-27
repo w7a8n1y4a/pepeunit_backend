a732059fa84c384a142fb4480b475da8bf515f63
Ivan Serebrennikov <admin@silberworks.com>
2025-05-27 04:20:31 +0300

    feat(click): add entity for pipe entry

---
+++ clickhouse/migrations/003_data_pipe_tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE n_last_entry
+(
+    uuid UUID,
+    unit_node_uuid UUID,
+    state String,
+    state_type Enum8('Number' = 1, 'Text' = 2),
+    create_datetime DateTime64(3),
+    max_count UInt32,
+    size UInt32,
+)
+ENGINE = MergeTree()
+ORDER BY (unit_node_uuid, create_datetime, uuid)
+PRIMARY KEY (unit_node_uuid, create_datetime)
+SETTINGS
+    merge_with_ttl_timeout = 600,
+    index_granularity = 128;
+
+CREATE TABLE window_entry
+(
+    uuid UUID,
+    unit_node_uuid UUID,
+    state String,
+    state_type Enum8('Number' = 1, 'Text' = 2),
+    create_datetime DateTime64(3),
+    expiration_datetime DateTime,
+    size UInt32
+)
+ENGINE = MergeTree()
+ORDER BY (unit_node_uuid, create_datetime, uuid)
+PRIMARY KEY (unit_node_uuid, create_datetime)
+TTL expiration_datetime
+SETTINGS
+    merge_with_ttl_timeout = 600,
+    index_granularity = 128;
+
+CREATE TABLE aggregation_entry
+(
+    uuid UUID,
+    unit_node_uuid UUID,
+    state Float64,
+    aggregation_type Enum8('avg' = 1, 'min' = 2, 'max' = 3, 'sum' = 4),
+    create_datetime DateTime64(3),
+    start_window_datetime DateTime64(3),
+    end_window_datetime DateTime64(3)
+)
+ENGINE = MergeTree()
+ORDER BY (unit_node_uuid, start_window_datetime, uuid)
+PRIMARY KEY (unit_node_uuid, start_window_datetime)
+SETTINGS
+    index_granularity = 128;
