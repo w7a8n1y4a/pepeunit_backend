3ca527a9c4e49e89e2a76ba7a2ad832d08676226
Ivan Serebrennikov <admin@silberworks.com>
2025-09-14 01:13:14 +0300

    ci(postgres): change version 17.2 to 17.6

---
+++ alembic/versions/2025_09_14_0103-5276216fd8d3_postges_version_change.py
@@ -0,0 +1,32 @@
+"""postges_version_change
+
+Revision ID: 5276216fd8d3
+Revises: b552dfb75bb7
+Create Date: 2025-09-14 01:03:57.018638
+
+"""
+from urllib.parse import urlparse
+
+from alembic import op
+from app import settings
+
+# revision identifiers, used by Alembic.
+revision = '5276216fd8d3'
+down_revision = 'b552dfb75bb7'
+branch_labels = None
+depends_on = None
+
+db_name = urlparse(settings.sqlalchemy_database_url).path.lstrip('/')
+
+def upgrade() -> None:
+
+    with op.get_context().autocommit_block():
+        op.execute(f"REINDEX DATABASE {db_name};")
+        op.execute(f"ALTER DATABASE {db_name} REFRESH COLLATION VERSION;")
+
+
+def downgrade() -> None:
+    with op.get_context().autocommit_block():
+        op.execute(f"REINDEX DATABASE {db_name};")
+        op.execute(f"ALTER DATABASE {db_name} REFRESH COLLATION VERSION;")
+
