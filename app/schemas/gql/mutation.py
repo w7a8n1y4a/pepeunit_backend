d0593bc7b07503933d06f30f3b6b92e89f0f2148
Ivan Serebrennikov <admin@silberworks.com>
2025-09-27 16:18:42 +0300

    ci(Dockerfile): add all extra to uv

--- app/schemas/gql/mutation.py
+++ app/schemas/gql/mutation.py
@@ -1,12 +1,52 @@
 from strawberry.tools import create_type
 
-from app.schemas.gql.mutations.grafana import *
-from app.schemas.gql.mutations.permission import *
-from app.schemas.gql.mutations.repo import *
-from app.schemas.gql.mutations.repository_registry import *
-from app.schemas.gql.mutations.unit import *
-from app.schemas.gql.mutations.unit_node import *
-from app.schemas.gql.mutations.user import *
+from app.schemas.gql.mutations.grafana import (
+    create_dashboard,
+    create_dashboard_panel,
+    delete_dashboard,
+    delete_link,
+    delete_panel,
+    link_unit_node_to_panel,
+    sync_dashboard,
+)
+from app.schemas.gql.mutations.permission import create_permission, delete_permission
+from app.schemas.gql.mutations.repo import (
+    create_repo,
+    update_repo,
+    delete_repo,
+    update_units_firmware,
+    bulk_update,
+)
+from app.schemas.gql.mutations.repository_registry import (
+    create_repository_registry,
+    set_credentials,
+    update_local_repository,
+    delete_repository_registry,
+)
+from app.schemas.gql.mutations.unit import (
+    create_unit,
+    update_unit,
+    update_unit_env,
+    set_state_storage,
+    send_command_to_input_base_topic,
+    delete_unit,
+)
+from app.schemas.gql.mutations.unit_node import (
+    create_unit_node_edge,
+    delete_unit_node_edge,
+    set_state_unit_node_input,
+    update_unit_node,
+    delete_data_pipe_data,
+    set_data_pipe_config,
+    set_data_pipe_data_csv,
+)
+from app.schemas.gql.mutations.user import (
+    create_user,
+    update_user,
+    block_user,
+    unblock_user,
+    delete_user_cookies,
+)
 
 Mutation = create_type(
     "Mutation",
--- app/schemas/gql/query.py
+++ app/schemas/gql/query.py
@@ -1,13 +1,46 @@
 from strawberry.tools import create_type
 
-from app.schemas.gql.queries.grafana import *
-from app.schemas.gql.queries.metrics import *
-from app.schemas.gql.queries.permission import *
-from app.schemas.gql.queries.repo import *
-from app.schemas.gql.queries.repository_registry import *
-from app.schemas.gql.queries.unit import *
-from app.schemas.gql.queries.unit_node import *
-from app.schemas.gql.queries.user import *
+from app.schemas.gql.queries.grafana import (
+    get_dashboard,
+    get_dashboards,
+    get_dashboard_panels,
+)
+from app.schemas.gql.queries.metrics import get_base_metrics
+from app.schemas.gql.queries.permission import get_resource_agents
+from app.schemas.gql.queries.repo import (
+    get_repo,
+    get_repos,
+    get_available_platforms,
+    get_versions,
+)
+from app.schemas.gql.queries.repository_registry import (
+    get_repository_registry,
+    get_branch_commits,
+    get_credentials,
+    get_repositories_registry,
+)
+from app.schemas.gql.queries.unit import (
+    get_unit,
+    get_unit_env,
+    get_target_version,
+    get_unit_current_schema,
+    get_state_storage,
+    get_units,
+    get_unit_logs,
+)
+from app.schemas.gql.queries.unit_node import (
+    get_unit_node,
+    get_pipe_data,
+    get_unit_nodes,
+    get_data_pipe_config,
+    check_data_pipe_config,
+)
+from app.schemas.gql.queries.user import (
+    get_user,
+    get_token,
+    get_verification_user,
+    get_users,
+)
 
 Query = create_type(
     "Query",
